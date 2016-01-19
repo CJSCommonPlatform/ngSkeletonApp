6c42f31ea1322a0443da97a53ff1becb85b2b3b4
Olu Alli <alleypa@yahoo.com>
2016-01-19 13:34:22 +0000

    Change build task to the correct clean task

--- gulp/build.js
+++ gulp/build.js
@@ -1,44 +1,43 @@
 'use strict';
 
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var runSequence = require('run-sequence');
-var gutil       = require('gulp-util');
+var gutil = require('gulp-util');
 
 // create standalone build
-module.exports = function(){
-  gulp.task('build', ['clean'], function(cb) {
-    if(gutil.env.production) {
-      runSequence([
-        'copy-images',
-        'build-css-app',
-        'build-css-vendor',
-        'build-js-app',
-        'build-js-vendor',
-        'copy-config',
-        'copy-favicon',
-        'copy-fonts',
-        'copy-html',
-        'copy-js-app',
-        'copy-cppui-assets',
-        'copy-localisation'
-      ], 'build-index', cb);
-    } else {
-      runSequence([
-        'copy-images',
-        'build-css-app',
-        'build-css-vendor',
-        //'cache-templates',
-        'copy-js-vendor',
-        'copy-config',
-        'copy-favicon',
-        'copy-fonts',
-        'copy-html',
-        'copy-js-app',
-        'copy-cppui-assets',
-        'copy-localisation'
-      ], 'build-index', cb);
-    }
-  });
+module.exports = function () {
+    gulp.task('build', ['clean-dist'], function (cb) {
+        if (gutil.env.production) {
+            runSequence([
+                'copy-images',
+                'build-css-app',
+                'build-css-vendor',
+                'build-js-app',
+                'build-js-vendor',
+                'copy-config',
+                'copy-favicon',
+                'copy-fonts',
+                'copy-html',
+                'copy-js-app',
+                'copy-cppui-assets',
+                'copy-localisation'
+            ], 'build-index', cb);
+        } else {
+            runSequence([
+                'copy-images',
+                'build-css-app',
+                'build-css-vendor',
+                //'cache-templates',
+                'copy-js-vendor',
+                'copy-config',
+                'copy-favicon',
+                'copy-fonts',
+                'copy-html',
+                'copy-js-app',
+                'copy-cppui-assets',
+                'copy-localisation'
+            ], 'build-index', cb);
+        }
+    });
 
 };
-

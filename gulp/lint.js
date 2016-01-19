d0b02b5104dbbe732a4c058ffe50c5c36f99bc56
Olu Alli <alleypa@yahoo.com>
2016-01-19 12:53:20 +0000

    Updated gulp lint with eslint rules

--- gulp/lint.js
+++ gulp/lint.js
@@ -6,38 +6,28 @@ var gulp = require('gulp'),
     map = require('map-stream'),
     $ = require('gulp-load-plugins')({ lazy: true });
 
-module.exports = function(config, log) {
-  //gulp.task('check-commit', ['lint-break', 'test']);
 
-  gulp.task('lint', function () {
-    log('Analysing source with JSHint and JSCS');
+module.exports = function (config, log) {
 
-    return lint(false);
-  });
+    gulp.task('check-commit', ['lint-break', 'test']);
 
-  gulp.task('lint-break', function () {
-    return lint(true);
-  });
+    gulp.task('lint', function () {
+        log('Analysing source with ESLint');
 
-  function lint(breakOnError) {
-    return gulp.src(config.globs.js)
-      .pipe($.if(args.verbose, $.print()))/*gulp lint --verbose*/
-      .pipe($.jshint())
-      .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
-      .pipe($.arialinter({
-        level: 'AA'
-      }))
-      .pipe($.if(breakOnError, jsHintErrorReporter()))
-      .pipe(browserSync.reload({stream: true}));
-  }
-
-  function jsHintErrorReporter() {
-    return map(function (file, cb) {
-      if (!file.jshint.success) {
-        console.log('Error: ' + file.path);
-        process.exit(1);
-      }
-      cb(null, file);
+        return lint(false);
     });
-  }
+
+    gulp.task('lint-break', function () {
+        return lint(true);
+    });
+
+    function lint(breakOnError) {
+        return gulp.src(config.globs.js)
+            .pipe($.if(args.verbose, $.print()))/*gulp lint --verbose*/
+            .pipe($.eslint()) // defaults to local linting config
+            .pipe($.eslint.format())
+            .pipe($.if(breakOnError, $.eslint.failOnError()))
+            .pipe(browserSync.reload({ stream: true }));
+    }
 };
+

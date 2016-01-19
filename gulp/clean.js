e28fa87266962ca78bf2a45cc0b3e8f8dff3b042
Olu Alli <alleypa@yahoo.com>
2016-01-19 12:55:45 +0000

    Updated the gulp clean task

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,12 +1,24 @@
 'use strict';
 
-var del  = require('del');
+var del = require('del');
 var gulp = require('gulp');
 
-// remove the /dist folder
-module.exports = function(cb){
-  gulp.task('clean', function(cb) {
-    del(['dist'], cb);
-  });
+module.exports = function (config, log) {
 
+    // remove the /dist folder
+    gulp.task('clean-dist', function (done) {
+        var files = [].concat(config.build_dir);
+        clean(files, done);
+    });
+
+    // this is used by deployment builds
+    gulp.task('clean-config', function (done) {
+        del(['dist/**/config.module.js'], done);
+    });
+
+    function clean(path, done) {
+        log('Cleaning: ' + path);
+        del(path, done);
+    }
 };
+

dc446c366920c17d0b279a019c68e58a80b0516c
Fabio Tisci <fabio.tapia@ext.soprasteria.com>
2016-02-08 17:03:43 +0000

    removed console log

--- gulp/copy-fonts.js
+++ gulp/copy-fonts.js
@@ -7,7 +7,6 @@ var $           = require('gulp-load-plugins')({ lazy: true });
 // copies all main fonts to /dist
 module.exports = function (config) {
   gulp.task('copy-fonts', function () {
-    console.log('files:', bowerFiles(config.fontFiles));
     return gulp.src(bowerFiles(config.fontFiles))
       .pipe($.rename({ dirname: ''}))
       .pipe(gulp.dest('dist/assets/fonts'))

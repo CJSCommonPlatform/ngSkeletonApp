6636cb432f5505d979d398b871f717fab9f59549
Fabio Tisci <fabio.tapia@ext.soprasteria.com>
2016-02-08 17:03:00 +0000

    solved Copy Fonts task is not working properly #4

--- gulp/copy-fonts.js
+++ gulp/copy-fonts.js
@@ -5,10 +5,12 @@ var bowerFiles  = require('main-bower-files');
 var $           = require('gulp-load-plugins')({ lazy: true });
 
 // copies all main fonts to /dist
-module.exports = function(config){
-  gulp.task('copy-fonts', function() {
+module.exports = function (config) {
+  gulp.task('copy-fonts', function () {
+    console.log('files:', bowerFiles(config.fontFiles));
     return gulp.src(bowerFiles(config.fontFiles))
-      .pipe(gulp.dest('dist/fonts'))
+      .pipe($.rename({ dirname: ''}))
+      .pipe(gulp.dest('dist/assets/fonts'))
       .pipe($.connect.reload());
   });
 

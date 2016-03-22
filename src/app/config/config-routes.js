ab18b1f2206a6d809149bfe1c55887811d6efff4
Adam Kwiatek <a.kwiatek@kainos.com>
2016-03-22 12:49:33 +0000

    Retab
    Get rid of jslint warnings about wrong tabbing.

--- src/app/config/config-routes.js
+++ src/app/config/config-routes.js
@@ -1,47 +1,47 @@
 (function () {
 
-  'use strict';
+    'use strict';
 
-  angular
-      .module('cpp-ui-spa-master.config.routes', [])
-      .provider('dynamicStateProvider', dynamicState);
+    angular
+        .module('cpp-ui-spa-master.config.routes', [])
+        .provider('dynamicStateProvider', dynamicState);
 
-  function dynamicState($stateProvider) {
-    this.$get = function () {
-      return {
-        /**
-         * @function app-ui-spa-master.config.routes
-         * @memberof app-ui-spa-master.config
-         * @param {string} state - a state object
-         * @author Fabio Tisci
-         * @description method to create states
-         */
-        addState: function (state) {
-          $stateProvider.state(state.state, {
-            url: state.url,
-            templateUrl: state.templateUrl,
-            data: {
-              pageTitle: state.pageTitle
-              //permissions:{
-              //  only: state.permissions
-              //}
-            },
-            controller: function (module, $controller) {
-              return $controller(state.controller);
-            },
-            controllerAs: state.controllerAs,
-            resolve:{
-              module: function ($ocLazyLoad){
-                return $ocLazyLoad.load({
-                  name: state.name,
-                  files: state.files,
-                  serie: true
-                });
-              }
-            }
-          });
-        }
-      };
-    };
-  }
+    function dynamicState($stateProvider) {
+        this.$get = function () {
+            return {
+                /**
+                 * @function app-ui-spa-master.config.routes
+                 * @memberof app-ui-spa-master.config
+                 * @param {string} state - a state object
+                 * @author Fabio Tisci
+                 * @description method to create states
+                 */
+                addState: function (state) {
+                    $stateProvider.state(state.state, {
+                        url: state.url,
+                        templateUrl: state.templateUrl,
+                        data: {
+                            pageTitle: state.pageTitle
+                            //permissions:{
+                            //    only: state.permissions
+                            //}
+                        },
+                        controller: function (module, $controller) {
+                            return $controller(state.controller);
+                        },
+                        controllerAs: state.controllerAs,
+                        resolve:{
+                            module: function ($ocLazyLoad){
+                                return $ocLazyLoad.load({
+                                    name: state.name,
+                                    files: state.files,
+                                    serie: true
+                                });
+                            }
+                        }
+                    });
+                }
+            };
+        };
+    }
 }());

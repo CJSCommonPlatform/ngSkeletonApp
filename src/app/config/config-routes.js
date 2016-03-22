8fa537071a35eb2ec3fdff41be71c16ff0cf47d7
James A Birrell Gray <james@jamesbirrellgray.com>
2016-03-22 11:07:44 +0000

    Update config-routes.js
    Fixes race conditions on lazy loading - requires you list your module dependencies before your controller in routes.config.json

--- src/app/config/config-routes.js
+++ src/app/config/config-routes.js
@@ -1,44 +1,47 @@
 (function () {
-    'use strict';
 
-    angular
-        .module('cpp-ui-spa-master.config.routes', [])
-        .provider('dynamicStateProvider', dynamicState);
+  'use strict';
 
-    function dynamicState($stateProvider) {
-        this.$get = function () {
-            return {
-                /**
-                 * @function app-ui-spa-master.config.routes
-                 * @memberof app-ui-spa-master.config
-                 * @param {string} state - a state object
-                 * @author Fabio Tisci
-                 * @description method to create states
-                 */
-                addState: function (state) {
-                    $stateProvider.state(state.state, {
-                        url: state.url,
-                        templateUrl: state.templateUrl,
-                        data: {
-                            pageTitle: state.pageTitle
-                            //permissions:{
-                            //  only: state.permissions
-                            //}
-                        },
-                        controller: state.controller,
-                        controllerAs: state.controllerAs,
-                        resolve: {
-                            state: function ($ocLazyLoad) {
-                                return $ocLazyLoad.load({
-                                    name: state.name,
-                                    files: state.files
-                                });
-                            }
-                        }
-                    });
-                }
-            };
-        };
-    }
+  angular
+      .module('cpp-ui-spa-master.config.routes', [])
+      .provider('dynamicStateProvider', dynamicState);
 
+  function dynamicState($stateProvider) {
+    this.$get = function () {
+      return {
+        /**
+         * @function app-ui-spa-master.config.routes
+         * @memberof app-ui-spa-master.config
+         * @param {string} state - a state object
+         * @author Fabio Tisci
+         * @description method to create states
+         */
+        addState: function (state) {
+          $stateProvider.state(state.state, {
+            url: state.url,
+            templateUrl: state.templateUrl,
+            data: {
+              pageTitle: state.pageTitle
+              //permissions:{
+              //  only: state.permissions
+              //}
+            },
+            controller: function (module, $controller) {
+              return new $controller(state.controller);
+            },
+            controllerAs: state.controllerAs,
+            resolve:{
+              module: function ($ocLazyLoad){
+                return $ocLazyLoad.load({
+                  name: state.name,
+                  files: state.files,
+                  serie: true
+                });
+              }
+            }
+          });
+        }
+      };
+    };
+  }
 }());
